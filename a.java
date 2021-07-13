a63f1bb992874615da657fb0a8c665d9d561f02d
muyeking <87261720+muyeking@users.noreply.github.com>
2021-07-13 12:35:44 +0800

    Add files via upload
    排列组合and计算奖金

---
+++ a.java
@@ -0,0 +1,23 @@
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c,d=0;
+		for(a=1;a<5;a++)
+		{
+			for(b=1;b<5;b++)
+			{
+				for(c=1;c<5;c++)
+				{
+					if(b!=a&&c!=a&&c!=b) {
+						System.out.printf("%d\n",a*100+b*10+c);
+					    d++;
+					}
+			    }
+		    }
+		
+	    }
+		System.out.println("能组成"+d+"个数");
+	}
+}
---
+++ b.java
@@ -0,0 +1,30 @@
+
+public class b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l=30;
+		double a=0;
+		if(l<=10) {
+			a=l*0.1;
+		}
+		else if(l>10&&l<=20) {
+			a=1+(l-10)*0.075;
+		}
+		else if(l>20&&l<=40) {
+			a=1+10*0.075+(l-20)*0.05;
+		}
+		else if(l>40&&l<=60) {
+			a=1+10*0.075+20*0.05+(l-40)*0.03;
+		}
+		else if(l>60&&l<=100) {
+			a=1+10*0.075+20*0.05+20*0.03+(l-60)*0.015;
+		}
+		else if(l>100) {
+			a=1+10*0.075+20*0.05+20*0.03+40*0.015+(l-100)*0.01;
+		}
+		System.out.print("应发放奖金");
+		System.out.printf("%.2f",a);
+	}
+
+}
